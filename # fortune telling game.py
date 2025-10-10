6b66fd694dc822ce128e1b0bb4b60d2915e70279
Babyskoll <paxton.shell@gmail.com>
2025-10-10 19:58:37 -0400

    feat: add replay option to fortune telling game with user prompt

--- # fortune telling game.py
+++ # fortune telling game.py
@@ -7,3 +7,9 @@ def tell_fortune():
 
 input("What is your desire, my child?" + "\n" + "> ")
 print("> " + tell_fortune())
+
+if input("Do you want to play again? (y/n) " + "\n" + "> ") == "n":
+    print("Well, you're no fun! Boo!")
+else:
+    input("What is your desire now, little one?" + "\n" + "> ")
+    print("> " + tell_fortune())
