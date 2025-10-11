f0b700ccd6776d79de1565ce39f6ec125044a9e5
Babyskoll <paxton.shell@gmail.com>
2025-10-10 20:29:40 -0400

    feat: add additional response handling and exit messages for fortune game

--- # fortune telling game.py
+++ # fortune telling game.py
@@ -11,5 +11,12 @@ print("> " + tell_fortune())
 if input("Do you want to play again? " + "\n" + "> ").lower() in ["yes", "y"]:
     input("What is your desire now, little one?" + "\n" + "> ")
     print("> " + tell_fortune())
-else:
+elif input in ["no", "n"]:
     print("Well, you're no fun! Boo!")
+elif input("I don't understand. Do you want to play again? " + "\n" + "> ").lower() in ["continue", "c", "yes", "y"]:
+    input("Okay! What is your desire now, little one?" + "\n" + "> ")
+    print("> " + tell_fortune())
+else:
+    print("Goodbye, my child.")
+    
+    
