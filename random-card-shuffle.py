62835b83fd6b81748627fd5dec4808408b1fcaa2
Babyskoll <paxton.shell@gmail.com>
2025-10-12 11:48:54 -0400

    created random card shuffler

---
+++ random-card-shuffle.py
@@ -0,0 +1,11 @@
+# random card shuffle
+import random
+
+# create a deck of cards
+deck = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+
+# shuffle the deck
+random.shuffle(deck)
+
+# print the shuffled deck
+print(deck)
