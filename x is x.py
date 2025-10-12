bfa8dd2e8e3f5183b055a049569a1e4edcba23b0
Babyskoll <paxton.shell@gmail.com>
2025-10-12 11:21:31 -0400

    refactor: simplify get_int() by removing break and print statements

--- x is x.py
+++ x is x.py
@@ -1,16 +1,12 @@
 def main():
-    x = get_int()
+    x = get_int("What is x? ")
     print(f"x is {x}")
 
-
-def get_int():
+def get_int(prompt):
     while True:
         try:
-            x = int(input("What is x? "))
+            return int(input(prompt))
         except ValueError:
-            print("x is not an integer")
-        else:
-            break
-    return x
+            pass    
 
 main()
