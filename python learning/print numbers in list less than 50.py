a07604a5b967e53bad80969e9edb764d60cac5bf
Babyskoll <paxton.shell@gmail.com>
2025-10-15 12:08:42 -0400

    submitting print numbers less than 50

---
+++ python learning/print numbers in list less than 50.py
@@ -0,0 +1,27 @@
+# make a list of random numbers
+import random
+list = [random.randint(1, 100) for i in range(20)]
+print(list)
+
+# make new list with numbers less than 5
+under_five = [i for i in list if i < 50]
+print(under_five)
+
+# ask for number
+number = int(input("Enter a number: "))
+
+# print numbers less than number
+for i in list:
+    if i < number:
+        print(i)
+
+# make new list with numbers less than number
+under_number = [i for i in list if i < number]
+print(under_number)
+
+# combine all functions into one
+import random
+list = [random.randint(1, 100) for i in range(20)]
+number = int(input("Enter a number: "))
+under_number = [i for i in list if i < number]
+print(under_number)
